123bb7fab63debaac80a51da377503cbef11a3f0
Benny Rochwerg <4321benny@gmail.com>
2024-01-20 01:24:52 -0500

    Cleaned the Outbreak First Known Case column

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -29,19 +29,39 @@ outbreak_cleaned_data <-
          `Outbreak First Known Cause` = `Causative Agent-1`) |>
   
   # Renaming the Type of Location entries
-  mutate(`Type of Location` = case_match(`Type of Location`,
-                                         "LTCH" ~
-                                           "Long-Term Care Home",
-                                         "Hospital-Acute Care" ~
-                                           "Hospital (Acute Care)",
-                                         "Retirement Home" ~
-                                           "Retirement Home",
-                                         "Hospital-Chronic Care" ~
-                                           "Hospital (Chronic Care)",
-                                         "Hospital-Psychiatric" ~
-                                           "Hospital (Psychiatric)",
-                                         "Transitional Care" ~
-                                           "Transitional Care"))
+  mutate(`Type of Location` =
+           case_match(`Type of Location`,
+                      "LTCH" ~ "Long-Term Care Home",
+                      "Hospital-Acute Care" ~ "Hospital (Acute Care)",
+                      "Retirement Home" ~ "Retirement Home",
+                      "Hospital-Chronic Care" ~ "Hospital (Chronic Care)",
+                      "Hospital-Psychiatric" ~ "Hospital (Psychiatric)",
+                      "Transitional Care" ~ "Transitional Care")) |>
+  
+  # Renaming some of the Outbreak First Known Cause entries for simplicity
+  mutate(`Outbreak First Known Cause` =
+           case_match(`Outbreak First Known Cause`,
+                      "COVID-19" ~ "COVID-19",
+                      "Parainfluenza" ~ "Parainfluenza",
+                      "Respiratory syncytial virus" ~
+                        "Respiratory syncytial virus",
+                      "Metapneumovirus" ~ "Metapneumovirus",
+                      "Norovirus" ~ "Norovirus",
+                      "Rhinovirus" ~ "Rhinovirus",
+                      "Group B Streptococcal disease (neonatal)" ~
+                        "Group B Streptococcal disease (neonatal)",
+                      "Enterovirus/Rhinovirus" ~ "Enterovirus/Rhinovirus",
+                      "Enterovirus" ~ "Enterovirus",
+                      "Streptococcus pyogenes" ~ "Streptococcus pyogenes",
+                      "Parainfluenza PIV III" ~ "Parainfluenza",
+                      "Coronavirus*" ~ "Seasonal coronavirus",
+                      "Influenza A (Not subtyped)" ~ "Influenza A",
+                      "Influenza A (H3)" ~ "Influenza A",
+                      "Influenza A (H1N1)" ~ "Influenza A",
+                      "CPE Enterobacter unspecified (NDM)" ~ "CPE",
+                      "CPE Unspecified (KPC)" ~ "CPE",
+                      "Pending" ~ "Pending/Unknown",
+                      "Unable to identify" ~ "Pending/Unknown"))
 
 #### Saving the Cleaned Dataset ####
 write_csv(outbreak_cleaned_data, "outputs/data/cleaned_data.csv")
